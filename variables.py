e3541ee383e0520b0ff8e7355925d08450888b24
Arzu KÜÇÜK <132925355+arzu336@users.noreply.github.com>
2024-07-21 12:12:43 +0300

    Create variables.py
    variables

---
+++ variables.py
@@ -0,0 +1,17 @@
+print(5000-(5000*0.27))
+
+# değişken tanımlama kuralları
+# rakam ile başlayamaz
+# büyük küçük harf duyarlılığı
+# türkçe karakter kullanmayalım
+# değişkenler arası boşluk olamaz
+
+x=1     #int
+y=2.3   #float
+name="Arzu"  #string
+isStudent= True #bool
+
+a='10'
+b='20'
+print(a+b)   #1020
+
