8579ce7875ee03a452f54a05e1e4802ae974b355
Arzu KÜÇÜK <132925355+arzu336@users.noreply.github.com>
2024-07-21 12:13:15 +0300

    Create variables-demo.py

---
+++ variables-demo.py
@@ -0,0 +1,14 @@
+customerName='Arzu'
+customerLastName='Küçük'
+customerGender=True  #kadın
+customerTC='12312123123'
+customerBirthyear=2003
+customerAddress='Ankara/Bala'
+customerAge=2023-customerBirthyear
+
+print(customerName+' '+customerLastName)
+print(customerGender)
+print(customerTC)
+print(customerBirthyear)
+print(customerAddress)
+print(customerAge)
